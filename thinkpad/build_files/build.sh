cab14567a19e41f52b5c6f69d07186b0376c899b
Matheus Zabin <matheusqueirozzabin@gmail.com>
2025-11-25 18:38:56 -0300

    feat: use toolbox

--- thinkpad/build_files/build.sh
+++ thinkpad/build_files/build.sh
@@ -2,7 +2,6 @@
 
 set -ouex pipefail
 
-dnf5 install -y dnf-plugins-core
 
 # Fingerprint setup, following:
 # https://www.reddit.com/r/Fedora/comments/1gj5jbn/get_fingerprint_authentication_working_fedora_41/
@@ -11,22 +10,6 @@ dnf5 install -y fprintd libfprint
 # Podman/Docker setup, podman is already installed in the base image:
 dnf5 install -y podman-docker docker-compose
 
-# VSCode install:
-dnf5 install -y "https://code.visualstudio.com/sha/download?build=stable&os=linux-rpm-x64"
-
-# Helix install:
-dnf5 install -y helix
-
-# Neovim install:
-dnf5 install -y neovim
-
-# Using Fish shell:
-dnf5 install -y fish util-linux util-linux-user
-chsh -s /usr/bin/fish
-
-# Installing stow:
-dnf5 install -y stow
-
 # Remove Firefox:
 dnf5 remove -y firefox
 
@@ -34,6 +17,8 @@ dnf5 remove -y firefox
 rm /opt # this is a file not a directory currently
 mkdir /opt # create the opt directory so files can be installed to it
 # install package (dnf5 -y install .....)
+
+dnf5 install -y dnf-plugins-core
 
 # Setup 1Password installation:
 dnf5 config-manager addrepo --from-repofile=/ctx/1password.repo
