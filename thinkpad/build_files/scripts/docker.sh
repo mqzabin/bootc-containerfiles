2719012b54eeb8964ce51297cb672de28a452006
mqzabin <me@mqzabin.dev>
2026-01-03 07:57:41 -0300

    fix: conflict

--- thinkpad/build_files/scripts/docker.sh
+++ thinkpad/build_files/scripts/docker.sh
@@ -5,11 +5,8 @@ dnf install -y \
     containerd \
     docker-compose \
     docker-compose-switch \
-    podman-docker \
     docker-switch \
     docker-buildx \
     moby-engine
 
 groupadd docker
-
-touch /etc/containers/nodocker
