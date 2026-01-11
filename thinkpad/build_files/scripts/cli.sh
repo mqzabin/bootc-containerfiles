d5f81091ce961250a3b0cfff9c86b24dbd5a2654
mqzabin <me@mqzabin.dev>
2026-01-11 10:23:36 -0300

    add mise

--- thinkpad/build_files/scripts/cli.sh
+++ thinkpad/build_files/scripts/cli.sh
@@ -4,12 +4,13 @@ set -ouex pipefail
 dnf5 copr enable -y dejan/lazygit
 dnf5 copr enable -y varlad/zellij 
 dnf5 copr enable -y lihaohong/yazi
-
+dnf copr enable jdxcode/mise
 
 dnf5 install -y \
     neovim \
     stow \
     lazygit \
     zellij \
-    yazi
+    yazi \
+    mise
 
